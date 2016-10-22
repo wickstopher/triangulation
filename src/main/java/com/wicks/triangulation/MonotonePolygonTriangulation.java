c32a5dc141c049c88466fd9d7e325cc34182a089
Christopher R. Wicks <wickstopher@gmail.com>
2016-10-22 18:18:55 -0400

    Horrible intermediary commit.

---
+++ src/main/java/com/wicks/triangulation/MonotonePolygonTriangulation.java
@@ -0,0 +1,14 @@
+package com.wicks.triangulation;
+
+/**
+ * Created by wickstopher on 10/22/16.
+ */
+public class MonotonePolygonTriangulation
+{
+    public MonotonePolygonTriangulation()
+    {
+
+    }
+
+    public
+}
